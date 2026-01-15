d55f9ba2a537895e1ffa878de789b7c414eca503
Lev Kuimov <levkuimov34@gmail.com>
2026-01-15 19:23:17 +0300

    feat: launched service

---
+++ src/EventService/Program.cs
@@ -0,0 +1,30 @@
+﻿using EventService.Application.Extensions;
+using EventService.Infrastructure.DataAccess.Extensions;
+using EventService.Presentation.Grpc.Extensions;
+using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+
+builder.Services.AddDatabaseOptions(builder.Configuration)
+    .AddNpgsqlDataSource()
+    .AddMigrations();
+
+builder.Services.AddInfrastructureRepositories();
+
+builder.Services.AddGrpcServices();
+
+WebApplication app = builder.Build();
+
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IMigrationRunner runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
+
+app.MapGrpcEndpoints();
+
+app.Run();
--- src/test/EventService/Program.cs
+++
@@ -1,9 +0,0 @@
-namespace EventService;
-
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello World!");
-    }
-}
